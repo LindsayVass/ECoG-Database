2acba9453cd938d952735a55a88b0c9898f4d751
LindsayVass <lindsay.k.morgan@gmail.com>
2015-10-30 12:57:20 -0700

    update chan_history structure

---
+++ m/functions/updateChanHistory.m
@@ -0,0 +1,19 @@
+function EEG = updateChanHistory(EEG, goodChanList, badChanList)
+% Update the EEG.chan_history structure to reflect changes to the list of
+% good and bad channels.
+%
+% Inputs:
+%   EEG: EEGLAB structure
+%   goodChanList: structure of good channel data, output by
+%       makeChannelLists.m
+%   badChanList: structure of bad channel data, output by
+%       makeChannelLists.m
+%
+% Output:
+%   EEG: updated EEGLAB structure
+
+histInd = length(EEG.chan_history) + 1;
+
+EEG.chan_history(histInd).date = datestr(now);
+EEG.chan_history(histInd).good_chans = goodChanList;
+EEG.chan_history(histInd).bad_chans = badChanList;
