e6cad2ec56f659020fad93ac4d89097dd38b3118
LindsayVass <lindsay.k.morgan@gmail.com>
2015-10-30 11:36:46 -0700

    save EEG file with structured filename

---
+++ m/functions/saveEEG.m
@@ -0,0 +1,26 @@
+function verID = saveEEG(EEG, outputDir, subjID, resID, verID)
+% Save EEG file using a filename that indicates the patient, researcher,
+% and version of the file.
+%
+% >> verID = saveEEG(EEG, outputDir, subjID, resID, verID)
+%
+% Inputs:
+%   EEG: EEGLAB structure to be saved
+%   outputDir: path to the output directory to save the data to
+%   subjID: string indicating the patient ID (e.g., 'TS071')
+%   resID: string indicating the researcher (e.g., 'LV')
+%   verID: integer indicating the current version of the file (e.g., [1])
+%
+% Output:
+%   verID: incremented version number
+
+%% format file name
+
+% if outputDir does not end with '/' then add it
+if outputDir(end) ~= '/'
+    outputDir = [outputDir '/'];
+end
+
+fileName = [outputDir subjID '_' resID '_' num2str(verID, '%03d') '.set'];
+
+pop_saveset(EEG, fileName);
