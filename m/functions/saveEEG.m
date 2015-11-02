711a5a638af3c0bdf2c2ee069ea5fb3a3c8697e4
LindsayVass <lindsay.k.morgan@gmail.com>
2015-11-02 10:47:29 -0800

    extend file naming scheme

--- m/functions/saveEEG.m
+++ m/functions/saveEEG.m
@@ -1,18 +1,31 @@
-function [verID, filePath] = saveEEG(EEG, outputDir, subjID, resID, verID)
+function [filePath] = saveEEG(EEG, outputDir, subjID, resID, chanVerID, refVerID, artVerID, epochVerID)
 % Save EEG file using a filename that indicates the patient, researcher,
-% and version of the file.
+% and version of the file. The version of the file is indicated with
+% respect to channel inclusion, referencing scheme, artifact exclusion, and
+% epoching scheme.
 %
-% >> verID = saveEEG(EEG, outputDir, subjID, resID, verID)
+% Ex: TS071_LV_C01_R02_A03_E01.set
+%   - Patient = TS071
+%   - Researcher = LV
+%   - Channels = version 01
+%   - Referencing = version 02
+%   - Artifacts = version 03
+%   - Epochs = version 01
+%
+% >> [filePath] = saveEEG(EEG, outputDir, subjID, resID, chanVerID, refVerID, artVerID, epochVerID)
 %
 % Inputs:
 %   EEG: EEGLAB structure to be saved
 %   outputDir: path to the output directory to save the data to
 %   subjID: string indicating the patient ID (e.g., 'TS071')
 %   resID: string indicating the researcher (e.g., 'LV')
-%   verID: integer indicating the current version of the file (e.g., [1])
+%   chanVerID: integer indicating the current version of channel
+%       inclusion/exclusion scheme
+%   refVerID: integer indicating the current version of referencing scheme
+%   artVerID: integer indicating the current version of artifact exclusion
+%   epochVerID: integer indicating the current version of epoching
 %
 % Output:
-%   verID: incremented version number
 %   filePath: path to the saved EEG dataset
 
 %% format file name
@@ -22,9 +35,18 @@ if outputDir(end) ~= '/'
     outputDir = [outputDir '/'];
 end
 
-% check that verID is an integer
-if isnumeric(verID) == 0
-    error('verID must be an integer');
+% check that IDs are integers
+if isnumeric(chanVerID) == 0
+    error('chanVerID must be an integer');
+end
+if isnumeric(refVerID) == 0
+    error('refVerID must be an integer');
+end
+if isnumeric(artVerID) == 0
+    error('artVerID must be an integer');
+end
+if isnumeric(epochVerID) == 0
+    error('epochVerID must be an integer');
 end
 
 % check that outputDir exists
@@ -32,15 +54,11 @@ if ~exist(outputDir, 'dir')
     error('Output directory (outputDir) does not exist.')
 end
 
-filePath = [outputDir subjID '_' resID '_' num2str(verID, '%03d') '.set'];
+filePath = [outputDir subjID '_' resID '_C' num2str(chanVerID, '%02d') '_R' num2str(refVerID, '%02d') '_A' num2str(artVerID, '%02d'), '_E' num2str(epochVerID, '%02d') '.set'];
 
-% check whether a file exists by this name. if so, increment verID
-while exist(filePath, 'file')
-    warning('A file exists with this name already. Incrementing the version number before saving.');
-    verID = verID + 1;
-    filePath = [outputDir subjID '_' resID '_' num2str(verID, '%03d') '.set'];
+% check whether a file exists by this name
+if exist(filePath, 'file')
+    error('A file exists with this name already. Check your version IDs.');
 end
 
 pop_saveset(EEG, filePath);
-
-verID = verID + 1;
