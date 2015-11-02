22a1cf992f68c84b54c87f64d6d31d4ca9e76bd2
LindsayVass <lindsay.k.morgan@gmail.com>
2015-11-02 11:01:19 -0800

    insert chanVerID into structure

--- m/functions/updateChanHistory.m
+++ m/functions/updateChanHistory.m
@@ -1,4 +1,4 @@
-function EEG = updateChanHistory(EEG, goodChanList, badChanList)
+function EEG = updateChanHistory(EEG, chanVerID, goodChanList, badChanList)
 % Update the EEG.chan_history structure to reflect changes to the list of
 % good and bad channels.
 %
@@ -6,6 +6,7 @@ function EEG = updateChanHistory(EEG, goodChanList, badChanList)
 %
 % Inputs:
 %   EEG: EEGLAB structure
+%   chanVerID: integer indicating current version of channel inclusion
 %   goodChanList: structure of good channel data, output by
 %       makeChannelLists.m
 %   badChanList: structure of bad channel data, output by
@@ -16,6 +17,7 @@ function EEG = updateChanHistory(EEG, goodChanList, badChanList)
 
 histInd = length(EEG.chan_history) + 1;
 
+EEG.chan_history(histInd).chanVerID = chanVerID;
 EEG.chan_history(histInd).date = datestr(now);
 EEG.chan_history(histInd).good_chans = goodChanList;
 EEG.chan_history(histInd).bad_chans = badChanList;
