6c2cb3203afdb4b67ed559688ee0b4290ec102ac
LindsayVass <lindsay.k.morgan@gmail.com>
2015-11-02 14:55:05 -0800

    initialize ID structure

---
+++ m/functions/makeIdStruct.m
@@ -0,0 +1,24 @@
+function id = makeIdStruct(patient, researcher)
+% Create a basic structure for storing file ID information.
+%
+% >> id = makeIdStruct(patient, researcher)
+%
+% Inputs:
+%   patient: string indicating the patient number ('TS071')
+%   researcher: string indicating the researcher's initials ('LV')
+%
+% Output:
+%   id: structure containing the following fields:
+%       - patient
+%       - researcher
+%       - channels (version number for channel exclusion
+%       - rereference (type of re-referencing scheme)
+%       - artifacts (version number for artifact exclusion)
+%       - epoch (type of epoching scheme)
+
+id.patient = patient;
+id.researcher = researcher;
+id.channels = 0;
+id.rereference = 'common';
+id.artifacts = 0;
+id.epoch = '';
