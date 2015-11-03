c966f7b7545092f899ed160a5e1e4a2176967a30
LindsayVass <lindsay.k.morgan@gmail.com>
2015-11-03 15:49:00 -0800

    setup vises only

--- m/functions/markBadChannels.m
+++ m/functions/markBadChannels.m
@@ -1,51 +1,26 @@
-function [EEG, varargout] = markBadChannels(EEG, varargin)
+function EEG = markBadChannels(EEG)
 
-% Mark bad channels that exhibit gross signal abnormalities as well as the
-% marker channel for data synchronization. These flags are stored in the
-% marks structure (EEG.marks.chan_info).
+% Prepare EEG for marking bad channels. This function will do the
+% following:
+%   - Initialize EEG.marks structure if one does not exist
+%   - Add "marker" label to EEG.marks.chan_info if one does not exist
+%   - Create a popup window with instructions for marking channels
+%   - Load the Vised EEG window
 %
-% Minimal example:
-% >> [EEG] = markBadChannels(EEG)
+% After 'Update EEG Structure' is clicked in EEG window, it will update
+% EEG.chan_history with the changes.
 %
-% Maximal example:
-% >> [EEG, id] = markBadChannels(EEG, id, VISED_CONFIG)
+% >> EEG = setupMarkChannels(EEG)
 %
 % Inputs:
 %   EEG: EEGLAB struct
-%
-% Optional Inputs:
-%   id: structure created by makeIdStruct.m
-%   VISED_CONFIG: configuration structure for vised_marks plugin; must be
-%       named 'VISED_CONFIG'
 %
 % Output:
 %   EEG: EEGLAB struct containing an updated marks structure with flags for
 %       bad channels and the marker channel
 %
-% Optional Output:
-%   id: updated id structure
 
 eeglab redraw;
-
-if nargin == 3
-    VISED_CONFIG = varargin{2};
-else
-    if nargin == 2
-        id = varargin{1};
-    end
-    % load default VISED_CONFIG
-    curPath = which('markBadChannels.m');
-    tbInd = strfind(curPath, 'ECoG Database/');
-    visedPath = [curPath(1:tbInd + 13) 'config/vised_config_marker_select.mat'];
-    load(visedPath);
-end
-
-% check that id.channels is an integer
-if exist('id', 'var')
-    if isnumeric(id.channels) == 0
-        error('id.channels must be an integer');
-    end
-end
 
 % initialize marks structure
 if isfield(EEG, 'marks') == 0
@@ -65,35 +40,6 @@ message = ['When the pop-up configuration window appears, select OK. \n\n' ...
 h = msgbox(sprintf(message), 'Mark Bad Channels', 'help');
 
 EEG = pop_vised(EEG);
-
-doneMarking = 0;
-while ~doneMarking
-end
-
-% after done marking channels...
-
-if doneMarking
-    % if anything changed
-    if isequal(origMarks, EEG.marks) == 0
-        if exist('id', 'var')
-            % increment channel version
-            id.channels = id.channels + 1;
-            varargout{1} = id;
-        end
-        
-        % create easy-to-read structures of good and bad channels
-        [goodChanList, badChanList] = makeChannelLists(EEG);
-        
-        % update chan_history
-        EEG = updateChanHistory(EEG, goodChanList, badChanList);
-    else
-        warning('No channels marked. Returning the same EEG.')
-        if exist('id', 'var')
-            varargout{1} = id;
-        end
-    end
-end
-
 
 
 
