1faa8bc40680051ac3a59d49c68f54f5c02c36f1
LindsayVass <lindsay.k.morgan@gmail.com>
2015-11-04 17:10:19 -0800

    allow user to supply bipolar structure

--- m/functions/rerefBipolar.m
+++ m/functions/rerefBipolar.m
@@ -1,4 +1,4 @@
-function [EEGreref, B] = rerefBipolar(EEG)
+function [EEGreref, B] = rerefBipolar(EEG, B)
 
 % Re-reference the data using a bi-polar referencing scheme that creates
 % virtual electrodes by differencing the signals from adjacent pairs of
@@ -19,6 +19,10 @@ function [EEGreref, B] = rerefBipolar(EEG)
 %       mean time series used to re-reference the data. If the EEGLAB data
 %       does not contain the EEG.marks structure, it will produce a warning
 %       and use all channels for re-referencing.
+%
+% Optional Input:
+%   B: structure containing bi-polar data (output by this file); if
+%       supplied, will be used instead of asking for user input
 %
 % Outputs:
 %   EEGreref: EEGLAB structure containing the re-referenced data.
@@ -53,66 +57,72 @@ chanNames  = {EEG.chanlocs.labels};
 stripNames = cellfun(@(x) x(regexp(x, '[^\d]')), chanNames, 'UniformOutput', false);
 stripList  = unique(stripNames);
 
+%% create bipolar structure
+if ~exist('B', 'var')
+    createB = 1;
+end
+
 %% ask user which strips/depths to perform bi-polar referencing on
-B = makeBipolarStruct(stripList);
-B = StructDlg(B);
-
+if createB
+    B = makeBipolarStruct(stripList);
+    B = StructDlg(B);
+end
 %% get user input for grid geometry
-
-global geomData;
-rerefStrips = {};
-for thisStrip = 1:length(stripList);
-    if B.(stripList{thisStrip}).Include == 1
-        rerefStrips(end + 1) = stripList(thisStrip);
-        
-        % make new geomGui window
-        geomGui(stripList(thisStrip));
-        h = findobj('Tag', 'GeomFigure');
-        uiwait(h);
-        
-        % check that there are no non-zero duplicate entries
-        geomDataNoZero = geomData(geomData ~= 0);
-        if length(unique(geomDataNoZero)) < length(geomDataNoZero(:))
-            error('Duplicate electrode entry in table.')
+if createB
+    global geomData;
+    rerefStrips = {};
+    for thisStrip = 1:length(stripList);
+        if B.(stripList{thisStrip}).Include == 1
+            rerefStrips(end + 1) = stripList(thisStrip);
+            
+            % make new geomGui window
+            geomGui(stripList(thisStrip));
+            h = findobj('Tag', 'GeomFigure');
+            uiwait(h);
+            
+            % check that there are no non-zero duplicate entries
+            geomDataNoZero = geomData(geomData ~= 0);
+            if length(unique(geomDataNoZero)) < length(geomDataNoZero(:))
+                error('Duplicate electrode entry in table.')
+            end
+            
+            % check that the inputted values match the electrode numbers
+            gridInds = find(strcmpi(stripNames, stripList{thisStrip}));
+            gridList = chanNames(gridInds);
+            gridNums = cellfun(@(x) str2num(x(regexp(x, '\d'))), gridList, 'UniformOutput', false);
+            gridNums = cat(1, gridNums{:});
+            if length(intersect(geomDataNoZero, gridNums)) < length(gridNums)
+                error('Inputted electrode numbers do not match those in EEG.')
+            end
+            
+            B.(stripList{thisStrip}).Geometry = geomData;
+            
         end
-        
-        % check that the inputted values match the electrode numbers
-        gridInds = find(strcmpi(stripNames, stripList{thisStrip}));
-        gridList = chanNames(gridInds);
-        gridNums = cellfun(@(x) str2num(x(regexp(x, '\d'))), gridList, 'UniformOutput', false);
-        gridNums = cat(1, gridNums{:});
-        if length(intersect(geomDataNoZero, gridNums)) < length(gridNums)
-            error('Inputted electrode numbers do not match those in EEG.')
-        end
-        
-        B.(stripList{thisStrip}).Geometry = geomData;
-        
     end
 end
 
 
-
 % %% update EEG.reref
- EEGreref = EEG;
+EEGreref = EEG;
 % EEGreref.reref.scheme = 'Strip/Depth';
 % EEGreref.reref.date   = datestr(now);
 % EEGreref.reref.chan.electrode_name = deal(chanNames)';
 % EEGreref.reref.chan.electrode_ind  = deal(1:1:EEG.nbchan)';
 % EEGreref.reref.chan.ref_ind = cell(EEG.nbchan, 1);
-% 
+%
 % %% re-reference the data
 % multiWaitbar('Re-referencing each strip/depth...', 0);
 % for thisStrip = 1:length(stripList)
 %     multiWaitbar('Re-referencing each strip/depth...', thisStrip / length(stripList));
-%     
+%
 %     % indices on this strip
 %     stripInds     = find(strcmpi(stripNames, stripList(thisStrip)));
 %     goodStripInds = intersect(stripInds, goodChanInds);
-%     
+%
 %     % calculate ref data
 %     refData = mean(EEG.data(goodStripInds, :), 1);
 %     EEGreref.data(goodStripInds, :) = EEGreref.data(goodStripInds, :) - repmat(refData, [length(goodStripInds) 1]);
-%     
+%
 %     % update EEG.reref.chan.ref_ind
 %     EEGreref.reref.chan.ref_ind(goodStripInds) = {goodStripInds};
 % end
